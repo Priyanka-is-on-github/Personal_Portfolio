d98af103d6de5d34c0254eeeddd04f38a4d12523
Priyanka Singh <priyankasingh8885@gmail.com>
2024-07-04 03:01:32 +0530

    Personal_Portfolio(3)

--- src/app/components/hooks/useScreenSize.jsx
+++ src/app/components/hooks/useScreenSize.jsx
@@ -1,24 +1,25 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const useScreenSize = () => {
-    const [screenSize, setScreenSize] = useState(window.innerWidth); // Initialize with current width
+    const [screenSize, setScreenSize] = useState(0); // Initialize with a default value
 
     useEffect(() => {
-        function handleResize() {
-            setScreenSize(window.innerWidth);
+        // Check if window is available
+        if (typeof window !== "undefined") {
+            const handleResize = () => setScreenSize(window.innerWidth);
+
+            window.addEventListener("resize", handleResize);
+            
+            // Set initial size
+            handleResize();
+
+            return () => window.removeEventListener("resize", handleResize);
         }
-
-        window.addEventListener("resize", handleResize);
-        
-        // Set initial size
-        handleResize();
-
-        return () => window.removeEventListener("resize", handleResize);
     }, []);
 
     return screenSize;
-}
+};
 
 export default useScreenSize;
